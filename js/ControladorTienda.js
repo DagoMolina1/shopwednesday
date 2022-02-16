ae3089603f4a5d335422487a2e0fe6b06075cb71
203 <203@academicos.cesde.edu.co>
2022-02-16 10:04:30 -0500

    se crea controlador

--- js/ControladorTienda.js
+++ js/ControladorTienda.js
@@ -1,1 +1,27 @@
-console.log("oe melossss")
+//Datos De Entrada (Comentario de linea: "//")
+/*
+Esto es un comentario de bloque:
+*/
+
+//ENTRADAS==VARIABLES==CONSTANTES
+let nombreProducto = 'batimovil'
+let precioProducto = 400000
+let pesoProducto = 18.5
+let tieneDescuento = true
+let descripcionProducto = 'Baimovil a escala'
+let telefonoVendedor = '3218217729'
+
+const NOMBRE_CLIENTE = "Dago Molina"
+
+//Arreglos unidimensionales (vectores) sirven par almacena varios elementos en un sola ariable
+
+let productos = ["Batimovil", "camisate", "gorra", "bati", "Bati Morral"]
+
+//Datos de Salida
+console.log("Buenas")
+console.log(nombreProducto)
+console.log("Sunombre es: " + NOMBRE_CLIENTE + " y su producto es " + nombreProducto)
+console.log("**********")
+console.log(productos)
+console.log(productos2)
+
